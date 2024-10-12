c284eb6c6658a3fff3e721aa264a496d26a3e693
Travis Claus <travisjohnclaus@gmail.com>
2024-10-12 19:35:24 -0400

    initial commit of flask app frame with simple template and js setup

---
+++ static/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  function hello() {
+    const root = document.getElementById("root");
+    root.innerHTML = "Hello World";
+  }
+
+  function clear() {
+    const root = document.getElementById("root");
+    root.innerHTML = null;
+  }
+
+  $("#printButton").click(hello());
+  $("#clearButton").click(() => clear());
+});
