7222d34a9c06d0f6a1b1402d934bdacd7ee45d28
Robert Hunt <finalrob@gmail.com>
2024-11-08 19:13:37 -0500

    Reformated the spacing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template("app.html")
+  return render_template("app.html")
 
 # Doesn't provide explicit error handling for none POST case.
 # Unsure if sepcifying only POST prevents GET requests,
@@ -13,28 +13,28 @@ def root():
 # behaves when we throw certains verbs and invalid formats at it.
 @app.route("/simulation", methods=['POST'])
 def simulation():
-    if request.method == 'POST':
-        # Naturally, form params are read from the html file; so their strings.
-        # Parse the values to int as we read them from the request.
-        matrixRows = int(request.form.get('matrixRows'))
-        matrixColumns = int(request.form.get('matrixColumns'))
-        sequenceLength = int(request.form.get('sequenceLength'))
+  if request.method == 'POST':
+    # Naturally, form params are read from the html file; so their strings.
+    # Parse the values to int as we read them from the request.
+    matrixRows = int(request.form.get('matrixRows'))
+    matrixColumns = int(request.form.get('matrixColumns'))
+    sequenceLength = int(request.form.get('sequenceLength'))
 
-        return GenArray([matrixRows, matrixColumns], sequenceLength).genArray
-    
+    return GenArray([matrixRows, matrixColumns], sequenceLength).genArray
+
 
 ### Rob's stuff ###
 
 
 # This routes to the page rob has been working on.
 # This page shows the intended design of the web-app.
-# We can change the route to "/" later.
+# We can change the route to the root "/" later.
 #
 @app.route("/home")
 def home():
   return render_template("home.html")
 
-# This code is just so I can run the program through the "run python file" button
-# in VScode.
+# This code is just so I can run the program through
+# the "run python file" button in VScode.
 if __name__ =="__main__":
   app.run(debug=True)
--- static/testapp.js
+++ static/testapp.js
@@ -2,30 +2,30 @@ document.addEventListener('DOMContentLoaded', pageLoaded);
 
 
 function pageLoaded() {
-    const simulationDiv = document.getElementById("simulationField");
-    const selectButton = document.getElementById("selectButton");
+  const simulationDiv = document.getElementById("simulationField");
+  const selectButton = document.getElementById("selectButton");
 
-    document.getElementById("startButton").addEventListener('click', startAction);
-    document.getElementById("stopButton").addEventListener('click', stopAction);
-    document.getElementById("newButton").addEventListener('click', newAction);
-    document.getElementById("selectButton").addEventListener('click', selectAction);
+  document.getElementById("startButton").addEventListener('click', startAction);
+  document.getElementById("stopButton").addEventListener('click', stopAction);
+  document.getElementById("newButton").addEventListener('click', newAction);
+  document.getElementById("selectButton").addEventListener('click', selectAction);
 
-    simulationDiv.innerHTML = "This is the simulation-field element called simulationDiv in javascript.";
+  simulationDiv.innerHTML = "This is the simulation-field element called simulationDiv in javascript.";
 };
 
 
 const startAction = () => {
-    return (document.getElementById("simulationField").innerHTML = "Start")
+  return (document.getElementById("simulationField").innerHTML = "Start")
 };
 
 const stopAction = () => {
-    return (document.getElementById("simulationField").innerHTML = "Stop")
+  return (document.getElementById("simulationField").innerHTML = "Stop")
 };
 
 const newAction = () => {
-    return (document.getElementById("simulationField").innerHTML = null)
+  return (document.getElementById("simulationField").innerHTML = null)
 };
 
 const selectAction = () => {
-    return (document.getElementById("simulationField").innerHTML = "Selecting...")
+  return (document.getElementById("simulationField").innerHTML = "Selecting...")
 };
